bd3576f2a45074b13423be92807d6814181776e7
catcuts <354151733@qq.com>
2017-07-19 01:28:11 +0800

    更新Paginator组件

--- src/components/Paginator.vue
+++ src/components/Paginator.vue
@@ -1,15 +1,17 @@
 <template>
   <div v-if="totalPageNo > 0" class="squares container" @click="onSquareClick($event)">
-    <div class="ui square segment"><span>{{ "<<" }}</span></div>
-    <div class="ui square segment"><span>{{ "<" }}</span></div>
+    <div class="ui square segment" data-pageno="1"><span>{{ "<<" }}</span></div>
+    <div class="ui square segment" data-pageno="prev"><span>{{ "<" }}</span></div>
+    <div v-if="showRollup" class="ui square segment" data-pageno="rollup"><span>{{ "..." }}</span></div>    
     <div class="ui square segment"
-      v-for="pageNo in totalPageNo" :key="pageNo"
-      :class="{ 'current': pageNo === 1 }"
+      v-for="itemNo in Math.min(totalPageNo, pageNoItemLimit)" :key="itemNo + veryLeftPageNo - 1" :data-pageno="itemNo + veryLeftPageNo - 1"
+      :class="{ 'current': itemNo + veryLeftPageNo - 1 === currentPageNo }"
     >
-			<span>{{ pageNo }}</span>
+			<span>{{ itemNo + veryLeftPageNo - 1 }}</span>
 		</div>
-    <div class="ui square segment"><span>{{ ">" }}</span></div>
-    <div class="ui square segment"><span>{{ ">>" }}</span></div>
+    <div v-if="showRolldown" class="ui square segment" data-pageno="rolldown"><span>{{ "..." }}</span></div>
+    <div class="ui square segment" data-pageno="next"><span>{{ ">" }}</span></div>
+    <div class="ui square segment" :data-pageno="totalPageNo"><span>{{ ">>" }}</span></div>
   </div>
 </template>
 
@@ -19,20 +21,41 @@ export default {
   mixins: [],
   data () {
     return {
-      page: 1,
-      totalPageNo: 10,
-      veryLeftPageNo: 1,
-      pageSize: 10
+      currentPageNo: this.initPageNo,
+      veryLeftPageNo: 1
     }
   },
   props: {
-    props: {type: Object, default: () => {}}
+    initPageNo: {type: Number, default: 1},
+    totalPageNo: {type: Number, default: 10},
+    pageNoItemLimit: {type: Number, default: 5}
   },
-  computed: {},
+  computed: {
+    showRollup () {
+      return !(this.veryLeftPageNo === 1)
+    },
+    showRolldown () {
+      return !(this.veryLeftPageNo === this.totalPageNo - this.pageNoItemLimit + 1)
+    }
+  },
   methods: {
     onSquareClick ($event) {
-      debugger
-      console.log($event)
+      var curPageNo =
+        $event.target.dataset.pageno ||
+        $event.target.parentElement.dataset.pageno
+      if (curPageNo === 'prev') {
+        this.currentPageNo = Math.max(1, this.currentPageNo - 1)
+        this.veryLeftPageNo = Math.max(1, this.currentPageNo - this.pageNoItemLimit + 1)
+      } else if (curPageNo === 'next') {
+        this.currentPageNo = Math.min(this.totalPageNo, this.currentPageNo + 1)
+        this.veryLeftPageNo = Math.max(1, this.currentPageNo - this.pageNoItemLimit + 1)
+      } else if (curPageNo === 'rollup') {
+        this.veryLeftPageNo = Math.max(1, this.veryLeftPageNo - 1)
+      } else if (curPageNo === 'rolldown') {
+        this.veryLeftPageNo = Math.min(this.totalPageNo - this.pageNoItemLimit + 1, this.veryLeftPageNo + 1)
+      } else if (curPageNo) {
+        this.currentPageNo = parseInt(curPageNo)
+      }
     },
     // 翻页器方法
     // 方法1：上滚页
@@ -101,13 +124,20 @@ export default {
   /* 专用 */
   .container {
     margin: 0 auto;
+    user-select: none;
   }
   /* 覆盖 */
   .square {
     display: inline-block;
     font-size: 1em!important;
+    font-weight: bolder;
     cursor: pointer;
+    user-select: none;
   }
+    .square.current {
+      background-color: #E5E5E5;
+      color: #FFF;
+    }
   /* 测试 */
   .test {
     margin: 0 auto;
